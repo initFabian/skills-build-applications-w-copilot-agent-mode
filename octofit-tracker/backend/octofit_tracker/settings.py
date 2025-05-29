4f86f41715d33914589adf9cac67aec7e41f3542
Fabian Buentello <fabian.buentello@gmail.com>
2025-05-29 02:05:25 +0000

    step 4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-48sh4%2nd9bd-*a2uf!zf+*3e_a5$vbq4c%$wa@l7f0tj8(u9y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "potential-succotash-5jxwpj97rqhp5w.github.dev"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "potential-succotash-5jxwpj97rqhp5w-8000.app.github.dev"]
 
 
 # Application definition
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "octofit_tracker",
     "rest_framework",
     "djongo",
     "corsheaders",
+    "octofit_tracker",
 ]
 
 MIDDLEWARE = [
@@ -51,9 +51,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
-
-MIDDLEWARE.insert(1, "corsheaders.middleware.CorsMiddleware")
 
 ROOT_URLCONF = "octofit_tracker.urls"
 
@@ -82,10 +81,8 @@ DATABASES = {
     "default": {
         "ENGINE": "djongo",
         "NAME": "octofit_db",
-        "CLIENT": {
-            "host": "localhost",
-            "port": 27017
-        }
+        "HOST": "localhost",
+        "PORT": 27017,
     }
 }
 
